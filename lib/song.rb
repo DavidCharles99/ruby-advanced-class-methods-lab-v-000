6b84872cba02809af61f82c06b6e81994eb2111f
David Charles <ratlover03@gmail.com>
2019-01-24 21:27:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,4 +51,13 @@ class Song
     new_song
   end
 
+  def self.create_from_filename(file)
+    new_file = file.split(" - ")
+    artist = new_file[0]
+    song = new_file[1].chomp(".mp3")
+    new_song = self.create
+    new_song.artist_name = artist
+    new_song.name = song
+    new_song
+  end
 end
